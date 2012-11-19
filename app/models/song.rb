62680e355b716a3504e71035bad6c59e5c0bdcd8
Jasmine Chabra <jchabra@gmail.com>
2012-11-19 17:25:53 -0500

    changes

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,16 +9,10 @@
 #  album_id   :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-<<<<<<< HEAD
 #  youtube    :string(255)
-=======
 #  song       :string(255)
-<<<<<<< HEAD
 #  genre_id   :integer
-=======
->>>>>>> 9c1a6e28de1d20157571434bb49524f4fc896bac
->>>>>>> b7b74bd9d72697415456978bf2c611710c928bfe
-#
+
 
 class Song < ActiveRecord::Base
 	belongs_to :album
